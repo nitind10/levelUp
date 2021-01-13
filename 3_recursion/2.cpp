e25f36eac9c2e2590481ead2f21e59a0b0100289
Nitin Dahiya <nitindahiya1010@gmail.com>
2021-01-13 14:07:27 +0530

    update

--- 3_recursion/2.cpp
+++ 3_recursion/2.cpp
@@ -1,7 +1,7 @@
 //permutation combination type questions
 
 //infinite coin permutations
-int permute(vector<int>& coins, int target, string ans){
+int infCoinPermute(vector<int>& coins, int target, string ans){
     if(target == 0){
         cout << ans << endl;
         return 1;
@@ -9,13 +9,13 @@ int permute(vector<int>& coins, int target, string ans){
     int count = 0;
     for(int i=0; i<coins.size(); ++i){
         if(target - coins[i] >= 0)
-            count += permute(coins, target - coins[i], ans + to_string(coins[i]));
+            count += infCoinPermute(coins, target - coins[i], ans + to_string(coins[i]));
     }
     return count;
 }
 
 //infinite coin combinations
-int combination(vector<int>& coins, int target, string ans, int idx){
+int infCoinCombination(vector<int>& coins, int target, string ans, int idx){
     if(target == 0){
         cout << ans << endl;
         return 1;
@@ -23,7 +23,39 @@ int combination(vector<int>& coins, int target, string ans, int idx){
     int count = 0;
     for(int i=idx; i<coins.size(); ++i){
         if(target - coins[i] >= 0)
-            count += combination(coins, target - coins[i], ans + to_string(coins[i]), i);
+            count += infCoinCombination(coins, target - coins[i], ans + to_string(coins[i]), i);
+    }
+    return count;
+}
+
+//single coin combinations
+int singleCoinCombination(vector<int>& coins, int target, string ans, int idx){
+    if(target == 0){
+        cout << ans << endl;
+        return 1;
+    }
+    int count = 0;
+    for(int i=idx; i<coins.size(); ++i){
+        if(target - coins[i] >= 0)
+            count += singleCoinCombination(coins, target - coins[i], ans + to_string(coins[i]), i+1);
+    }
+    return count;
+}
+
+//single coin permutations
+int singleCoinPermute(vector<int>& coins, int target, string ans){
+    if(target == 0){
+        cout << ans << endl;
+        return 1;
+    }
+    int count = 0;
+    for(int i=0; i<coins.size(); ++i){
+        if(coins[i] > 0 && target - coins[i] >= 0){
+            int ele = coins[i];
+            coins[i] *= -1;
+            count += singleCoinPermute(coins, target - ele, ans + to_string(ele));
+            coins[i] *= -1;
+        }
     }
     return count;
 }
