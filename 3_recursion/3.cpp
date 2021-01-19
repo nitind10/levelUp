67eeae948439a0028094ace3cd8b39a9f8f38645
Nitin Dahiya <nitindahiya1010@gmail.com>
2021-01-19 20:14:05 +0530

    update

--- 3_recursion/3.cpp
+++ 3_recursion/3.cpp
@@ -326,3 +326,13 @@ int nQueens5(int r, int n){
 int totalNQueens(int n) {
     return nQueens5(0, n);
 }
+
+//191 counting set bits
+int hammingWeight(uint32_t n) {
+    int count = 0;
+    while(n != 0){
+        count++;
+        n &= (n - 1);
+    }
+    return count;
+}
