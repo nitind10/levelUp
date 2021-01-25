7624c7029c35f187e851b0ea69a93178e454aa23
Nitin Dahiya <nitindahiya1010@gmail.com>
2021-01-25 12:34:03 +0530

    update

--- 4_linkedList/1.cpp
+++ 4_linkedList/1.cpp
@@ -54,3 +54,48 @@ ListNode* reverseList(ListNode* head) {
     
     return reverseRec(head);
 }
+
+//234
+ListNode* reverseList(ListNode* head){
+    ListNode *prev = nullptr, *curr = head, *forw = head;
+    while(curr != nullptr){
+        forw = curr -> next;
+        curr -> next = prev;
+        prev = curr;
+        curr = forw;
+    }
+    return prev;
+}
+
+ListNode* middle(ListNode* head){
+    ListNode *slow = head, *fast = head;
+    while(fast -> next != nullptr && fast -> next -> next != nullptr){
+        slow = slow -> next;
+        fast = fast -> next -> next;
+    }
+    return slow;
+}
+
+bool isPalindrome(ListNode* head) {
+    if(head == nullptr || head -> next == nullptr)
+            return true;
+    
+    ListNode *mid = middle(head);
+    ListNode *head2 = mid -> next;
+    mid -> next = nullptr;
+    
+    head2 = reverseList(head2);
+    
+    ListNode *temp1 = head, *temp2 = head2;
+    
+    while(temp1 != nullptr && temp2 != nullptr){
+        if(temp1 -> val != temp2 -> val)
+            return false;
+        temp1 = temp1 -> next;
+        temp2 = temp2 -> next;
+    }
+
+    mid -> next = reverseList(head2);
+    
+    return true;
+}
