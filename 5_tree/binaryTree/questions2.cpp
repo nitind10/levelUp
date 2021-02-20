d0c43fca5f6ed4de19176da495ab477f64064a54
Nitin Dahiya <nitindahiya1010@gmail.com>
2021-02-20 11:44:57 +0530

    update

--- 5_tree/binaryTree/questions2.cpp
+++ 5_tree/binaryTree/questions2.cpp
@@ -99,4 +99,85 @@ void levelOrder4(TreeNode* node){
     }
 }
 
+//102 ===============================================================================
+vector<vector<int>> findLevels(TreeNode* root){
+    vector<vector<int>> ans;
+    int level = 0;
+    queue<TreeNode*> q;
+    q.push(root);
+    
+    while(!q.empty()){
+        int size_ = q.size();
+        
+        while(size_--){
+            TreeNode *rn = q.front();
+            q.pop();
+            if(level == ans.size())
+                ans.push_back({});
+            ans.back().push_back(rn -> val);
+            
+            if(rn -> left != nullptr) q.push(rn -> left);
+            if(rn -> right != nullptr) q.push(rn -> right);
+        }
+        level++;
+    }
+    
+    return ans;
+}
+
+//199 =====================================================================================
+vector<int> rightSideView(TreeNode* root) {
+    if(root == nullptr) return {};
+    vector<int> ans;
+
+    queue<TreeNode*> q;
+    q.push(root);
+    
+    while(!q.empty()){
+        int size_ = q.size();
+        ans.push_back(q.front() -> val);
+        
+        while(size_--){
+            TreeNode *rn = q.front();
+            q.pop();
+            
+            if(rn -> right != nullptr) q.push(rn -> right);
+            if(rn -> left != nullptr) q.push(rn -> left);
+        }
+    }
+    return ans;
+}
+
+//top view of tree ==============================================================================
+void topView(TreeNode* root){
+            //node, its width idx
+    queue<pair<TreeNode*, int>> q;
+    q.push({root, 0});
+    
+                //width idx, all nodes' vals with that width idx: level wise
+    unordered_map<int, vector<int>> map;
+    
+    int minWidthIdx = 1e9, maxWidthIdx = -1e9;
+    
+    while(!q.empty()){
+        pair<TreeNode*, int> rp = q.front();
+        q.pop();
+        
+        map[rp.second].push_back(rp.first -> val);
+        
+        minWidthIdx = min(minWidthIdx, rp.second);
+        maxWidthIdx = max(maxWidthIdx, rp.second);
+        
+        if(rp.first -> left != nullptr) 
+            q.push({rp.first -> left, rp.second - 1});
+        if(rp.first -> right != nullptr) 
+            q.push({rp.first -> right, rp.second + 1});
+    }
+    
+    vector<vector<int>> res;
+    
+    while(minWidthIdx <= maxWidthIdx)
+        res.push_back(map[minWidthIdx++]);
+}
+
 
