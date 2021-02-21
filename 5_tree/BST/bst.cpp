e1cc4b113c9e462c1632e0ede841502407840d41
Nitin Dahiya <nitindahiya1010@gmail.com>
2021-02-21 13:11:15 +0530

    update

--- 5_tree/BST/bst.cpp
+++ 5_tree/BST/bst.cpp
@@ -49,3 +49,64 @@ TreeNode* insertIntoBST(TreeNode* root, int val) {
 
     return root;
 }
+
+//450 ====================================================================================
+int maximumOfTree(TreeNode* node){
+    TreeNode *temp = node;
+    while(temp -> right != nullptr)
+        temp = temp -> right;
+    return temp -> val;
+}
+
+TreeNode* deleteNode(TreeNode* root, int key) {
+    if(root == nullptr)
+        return root;
+    if(root -> val > key)
+        root -> left = deleteNode(root -> left, key);
+    else if(root -> val < key)
+        root -> right = deleteNode(root -> right, key);
+    //key found
+    else{
+        //leaf node, single parent (both cases) handeled
+        if(root -> left == nullptr || root -> right == nullptr)
+            return root -> left == nullptr ? root -> right : root -> left;
+        
+        //node has both sub trees
+        int maxOfLST = maximumOfTree(root -> left);
+        root -> val = maxOfLST;
+        root -> left = deleteNode(root -> left, maxOfLST); 
+    }
+    return root;
+}
+
+//235 ==============================================================
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+    TreeNode *temp = root;
+    while(temp != nullptr){
+        if(temp -> val > p -> val && temp -> val > q -> val)
+            temp = temp -> left;
+        else if(temp -> val < p -> val && temp -> val < q -> val)
+            temp = temp -> right;
+        //cant decide where to go => this is the LCA
+        else
+            return temp;
+    }
+    return nullptr;
+}
+
+//108 =======================================================================
+TreeNode* createBST(vector<int>& nums, int si, int ei){
+    if(si > ei)
+        return nullptr;
+    
+    int mid = (si + ei)/2;
+    TreeNode* node = new TreeNode(nums[mid]);
+    
+    node -> left = createBST(nums, si, mid - 1);
+    node -> right = createBST(nums, mid + 1, ei);
+    
+    return node;
+}
+TreeNode* sortedArrayToBST(vector<int>& nums) {
+    return createBST(nums, 0, nums.size() - 1);
+}
