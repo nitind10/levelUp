952d0536297b6051f132762a2ddcbcea34d206d6
Nitin Dahiya <nitindahiya1010@gmail.com>
2021-03-18 22:26:46 +0530

    update

--- 5_tree/binaryTree/gTree.java
+++ 5_tree/binaryTree/gTree.java
@@ -54,6 +54,24 @@ public class gTree {
         return false;
     }
 
+    //depth of a node ========================================================================
+    public static int find01(Node node, int data) {
+        if (node.val == data)
+            return 0;
+
+        int depth = -1;
+        for (Node child : node.childs) {
+            depth = find01(child, data);
+            if (depth != -1)
+                break;
+        }
+
+        if (depth != -1)
+            depth++;
+
+        return depth;
+    }
+
     //node to root path ====================================================
     public static boolean nodeToRootPath(Node node, ArrayList<Node> ans, int data){
         if(node.val == data){
@@ -86,6 +104,60 @@ public class gTree {
             j--;
         }
         return lca;
+    }
+
+    //burning tree ===================================================================================================
+    public void kDown(Node root, Node blockNode, int time, List<List<Integer>> ans) {
+        if (root == blockNode)
+            return;
+
+        if (ans.size() == time)
+            ans.add(new ArrayList<>());
+
+        ans.get(time).add(root.val);
+        for (Node child : root.childs)
+            kDown(child, blockNode, time + 1, ans);
+
+    }
+
+    // Method_01
+    public List<List<Integer>> burningTree_01(Node root, int target) {
+        ArrayList<Node> list = new ArrayList<>();
+        rootToNodePath(root, target, list);
+
+        List<List<Integer>> ans = new ArrayList<>();
+        Node blockNode = null;
+
+        for (int i = 0; i < list.size(); i++) {
+            kDown(list.get(i), blockNode, i, ans);
+            blockNode = list.get(i);
+        }
+
+        return ans;
+    }
+
+    // Method_02
+    public int burningTree_02(Node root, int target, List<List<Integer>> ans) {
+        if (root.val == target) {
+            kDown(root, null, 0, ans);
+            return 1;
+        }
+
+        int time = -1;
+        Node blockNode = null;
+        for (Node child : root.childs) {
+            time = burningTree_02(child, target, ans);
+            if (time != -1) {
+                blockNode = child;
+                break;
+            }
+        }
+
+        if (time != -1) {
+            kDown(root, blockNode, time, ans);
+            time++;
+        }
+        return time;
     }
 
 
