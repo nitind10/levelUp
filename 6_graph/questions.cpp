3894f67bf206049785740d657c4a87f07bd425d8
Nitin Dahiya <nitindahiya1010@gmail.com>
2021-04-24 09:47:47 +0530

    update

--- 6_graph/questions.cpp
+++ 6_graph/questions.cpp
@@ -865,7 +865,7 @@ int mainForThisQues(){
     return -1;
 }
 
-//743 =====================================================
+//743 ==========================================================================================================================
 int networkDelayTime(vector<vector<int>> &times, int n, int k)
 {
     vector<vector<pair<int, int>>> graph(n + 1);
@@ -916,4 +916,94 @@ int networkDelayTime(vector<vector<int>> &times, int n, int k)
         return -1;
 
     return maxValue;
+}
+
+//1192 ============================================================================================
+vector<int> dis, low;
+vector<bool> vis;
+int time = 0;
+vector<vector<int>> res;
+
+void dfs(int src, int par, int n, vector<vector<int>> &graph)
+{
+    dis[src] = low[src] = time++;
+    vis[src] = true;
+
+    for (int nbr : graph[src])
+    {
+        if (!vis[nbr])
+        {
+            dfs(nbr, src, n, graph);
+
+            if (dis[src] < low[nbr])
+                res.push_back({src, nbr});
+
+            low[src] = min(low[src], low[nbr]);
+        }
+        else if (nbr != par)
+            low[src] = min(dis[nbr], low[src]);
+    }
+}
+
+vector<vector<int>> criticalConnections(int n, vector<vector<int>> &connections)
+{
+    vector<vector<int>> graph(n);
+    for (vector<int> &ar : connections)
+    {
+        graph[ar[0]].push_back(ar[1]);
+        graph[ar[1]].push_back(ar[0]);
+    }
+
+    dis.resize(n, 0);
+    low.resize(n, 0);
+    vis.resize(n, false);
+
+    dfs(0, -1, n, graph);
+    return res;
+}
+
+//787 ===================================================================================================================
+class Edge
+    {
+    public:
+        int v, w;
+        Edge(int v, int w)
+        {
+            this->v = v;
+            this->w = w;
+        }
+    };
+    
+    int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int K)
+{
+    vector<vector<Edge>> graph(n);
+
+    for (vector<int> &ar : flights)
+    {
+        graph[ar[0]].push_back(Edge(ar[1], ar[2]));
+    }
+
+    // {wsf,stop,src}
+    priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+
+    vector<bool> vis(n, false);
+    pq.push({0, K + 1, src});
+    while (pq.size() != 0)
+    {
+        vector<int> rvtx = pq.top();
+        pq.pop();
+        int vtx = rvtx[2], wsf = rvtx[0], edgeCount = rvtx[1];
+
+        if (vtx == dst)
+            return wsf;
+        if (edgeCount <= 0)
+            continue;
+
+        for (Edge e : graph[vtx])
+        {
+            pq.push({e.w + wsf, edgeCount - 1, e.v});
+        }
+    }
+
+    return -1;
 }
