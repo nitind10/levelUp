6f10eb8ee22a1b27d3403d976a34e53a92783204
Nitin Dahiya <nitindahiya1010@gmail.com>
2021-05-28 18:07:21 +0530

    update

--- 8_stack/questions.cpp
+++ 8_stack/questions.cpp
@@ -139,3 +139,26 @@ class StockSpanner {
             return span;
         }
 };
+
+
+//20 ================================================================
+ bool isValid(string s) {
+        stack<char> st;
+        
+        for(int i = 0; i < s.length(); ++i){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{')
+                st.push(s[i]);
+            else{
+                if(st.size() == 0)
+                    return false;
+                
+                if((s[i] == ')' && st.top() != '(') || (s[i] == ']' && st.top() != '[') || (s[i] == '}' && st.top() != '{'))
+                    return false;
+                else
+                    st.pop();
+            }
+        }
+        if(st.size() != 0)
+            return false;
+        return true;
+    }
